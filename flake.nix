ad661c37dbe7ca5befbc2a031bd37ced8ba010ae
very-blank <aapeli.saarelainen.76@gmail.com>
2026-01-16 00:45:58 +0200

    feat: Changes.

--- flake.nix
+++ flake.nix
@@ -14,65 +14,9 @@
   # config.scheme.base0${"0"-"F"}
 
   outputs = {...} @ inputs: {
-    nixosModules.default = {
-      lib,
-      config,
-      ...
-    }: {
-      import = inputs.base16.nixosModule;
-
-      options = {
-        colors = {
-          theme = lib.mkOption {
-            default = "tokyo-night-terminal-dark";
-            description = "The base 16 seceme used for theming.";
-            type = lib.types.enum [
-              "catppuccin-mocha"
-              "chinoiserie-midnight"
-              "atelier-plateau"
-              "tokyo-night-terminal-dark"
-            ];
-          };
-
-          overrides = lib.mkOption {
-            default = [];
-
-            type = lib.types.listOf (
-              lib.types.submodule {
-                options = {
-                  color = lib.mkOption {
-                    type = lib.types.str;
-                    description = "The name of the color.";
-                    example = "base00";
-                  };
-
-                  value = lib.mkOption {
-                    type = lib.types.str;
-                    description = "Hex value of the color.";
-                  };
-                };
-              }
-            );
-          };
-        };
-      };
-
-      config = let
-        cfg = config.modules.graphical.theming;
-        isOverridesZero = builtins.length cfg.overrides == 0;
-      in
-        lib.mkMerge [
-          (lib.mkIf isOverridesZero {scheme = "${inputs.tt-schemes}/base16/${cfg.theme}.yaml";})
-          (lib.mkIf (!isOverridesZero) {
-            scheme = (
-              (config.lib.base16.mkSchemeAttrs "${inputs.tt-schemes}/base16/${cfg.theme}.yaml").override (
-                lib.attrsets.genAttrs' cfg.overrides (
-                  option: lib.attrsets.nameValuePair (option.color) (option.value)
-                )
-              )
-            );
-          })
-        ];
+    nixosModules = {
+      default = import ./default.nix;
+      specialArgs = {inherit inputs;};
     };
   };
 }
