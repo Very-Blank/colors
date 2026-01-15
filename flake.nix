8877aba4421bafba5f3f6fd0d7abd0c04e05e1b3
very-blank <aapeli.saarelainen.76@gmail.com>
2026-01-15 23:15:14 +0200

    feat: Changes.

--- flake.nix
+++ flake.nix
@@ -14,10 +14,12 @@
   # To actually use the colors use:
   # config.scheme.base0${"0"-"F"}
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    nixosModules."colors" = {config, ...}: let
-      lib = nixpkgs.lib;
-    in {
+  outputs = {...} @ inputs: {
+    nixosModules."colors" = {
+      config,
+      lib,
+      ...
+    }: {
       imports = [
         inputs.base16.nixosModule
       ];
