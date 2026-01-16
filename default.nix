43bd7cb0353725f426b6f781b4e546768b2c454a
very-blank <aapeli.saarelainen.76@gmail.com>
2026-01-16 13:06:19 +0200

    feat: Changes.

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@
     colors = {
       name = lib.mkOption {
         default = "tokyodark";
-        description = "One of the base16 colors.";
+        description = "Name of the theme.";
         type = lib.types.nonEmptyStr;
       };
 
@@ -63,7 +63,7 @@
   config = let
     cfg = config.colors;
   in {
-    colors.theme = (import ./base16 ++ "/${cfg.name}.nix").palette;
+    colors.theme = (import (./base16 ++ "/${cfg.name}.nix")).palette;
   };
   # lib.mkIf ((builtins.length cfg.overrides) != 0) {
   #   colors.theme =
